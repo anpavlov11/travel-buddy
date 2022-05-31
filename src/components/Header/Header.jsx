1bb563269fc170d550fb1ffdf6010a0c268db3e7
anpavlov11 <apavlo11@gmail.com>
2022-05-31 00:39:26 -0500

    added Header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons';
-import { CallMissedSharp } from '@material-ui/icons';
+import SearchIcon from '@material-ui/icons/Search';
 import useStyles from './styles';
 
 const Header = () => {
     const classes = useStyles();
     return (
         <AppBar position='static'>
-            <Toolbar className={CallMissedSharp.toolbar}>
+            <Toolbar className={classes.toolbar}>
                 <Typography variant='h5' className={classes.title}>
                     Travel Buddy
                 </Typography>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,13 +1,14 @@
 import { alpha, makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-      title: {
+    title: {
+        fontFamily: 'courier-new',
         display: 'none',
         [theme.breakpoints.up('sm')]: {
-            display: 'block',
+        display: 'block',
         },
-      },
-      search: {
+    },
+    search: {
         position: 'relative',
         borderRadius: theme.shape.borderRadius,
         backgroundColor: alpha(theme.palette.common.white, 0.15),
@@ -16,17 +17,30 @@ export default makeStyles((theme) => ({
         marginLeft: 0,
         width: '100%',
         [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
-      },
-      searchIcon: {
-        padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
-      },
-      inputRoot: {
-        color: 'inherit',
-      },
-      inputInput: {
-        padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
-      },
-      toolbar: {
-        display: 'flex', justifyContent: 'space-between',
-      },
-    }));
+    },
+    searchIcon: {
+        color: 'yellow', 
+        padding: theme.spacing(0, 2), 
+        height: '100%', 
+        alignItems: 'center', 
+        pointerEvents: 'none', 
+        display: 'flex', 
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        fontFamily: 'courier-new',
+        color: 'white',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0), 
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, 
+        transition: theme.transitions.create('width'), 
+        width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
+    },
+    toolbar: {
+        fontFamily: 'courier-new',
+        color: 'yellow',
+        display: 'flex', 
+        justifyContent: 'space-between',
+    },
+    }));    
